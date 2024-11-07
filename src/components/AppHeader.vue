4a8f542f2eb21ec0bcdfc3f55544fdb8f32278fc
Maksym Savchuk <m.savchuk@urancompany.com>
2024-11-08 00:11:04 +0200

    create header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <LangSwitch />
+
+    <MainLogo class="w-11 xs:w-16 xl:w-20 mx-auto" />
+
+    <MenuButton class="h-2.5 sm:hidden cursor-pointer" />
+  </header>
+</template>
+
+<style lang="postcss" scoped>
+.header {
+  @apply flex items-center relative p-4 xl:p-6;
+}
+</style>
